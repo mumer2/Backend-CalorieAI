35d487ac67d9d4cbb8dcf0e7437209be965053d3
mumer2 <umermalik8280@gmail.com>
2025-06-23 09:40:37 +0500

    db

--- db.js
+++ db.js
@@ -1,4 +1,3 @@
-// db.js
 const mongoose = require('mongoose');
 
 let isConnected = false;
@@ -6,11 +5,7 @@ let isConnected = false;
 const connectToDatabase = async () => {
   if (isConnected) return;
 
-  await mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
-
+  await mongoose.connect(process.env.MONGO_URI);
   isConnected = true;
 };
 
