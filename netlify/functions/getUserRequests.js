508977345630b13f198ac6dadcee5495f1e9c862
mumer2 <umermalik8280@gmail.com>
2025-06-23 10:24:20 +0500

    getrequest

---
+++ netlify/functions/getUserRequests.js
@@ -0,0 +1,22 @@
+// netlify/functions/getUserRequests.js
+const connectDB = require('./db');
+const Request = require('./models/Request');
+
+exports.handler = async (event) => {
+  await connectDB();
+
+  const { userId } = event.queryStringParameters;
+
+  try {
+    const requests = await Request.find({ userId }).sort({ createdAt: -1 });
+    return {
+      statusCode: 200,
+      body: JSON.stringify(requests),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Server error' }),
+    };
+  }
+};
